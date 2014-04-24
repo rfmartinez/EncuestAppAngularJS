f28b6e153afb38208e52834b643d45695f299a67
Rafael Martinez <rafael.antonio.martinezs@gmail.com>
2014-04-24 12:59:32 -0500

    api rest slim microframework notorm
    api rest slim microframework notorm

--- api/index.php
+++ api/index.php
@@ -1,7 +1,5 @@
 ﻿<?php
 
-session_start();
-
 require 'vendor/autoload.php';
 
 use Mailgun\Mailgun;
@@ -48,10 +46,10 @@ function generarCodigo($longitud) {
  */
 require "NotORM.php";
  $servidor= "172.0.0.1";
-$user="deelepah_nuser";
-$pass="+UVpbqt{ZA.s";
+$user="root";
+$pass="";
  
-$pdo = new PDO("mysql:dbname=deelepah_ndb;host=127.0.0.1", $user, $pass);
+$pdo = new PDO("mysql:dbname=encuesta;host=127.0.0.1", $user, $pass);
 $db = new NotORM($pdo);
 
 
@@ -59,439 +57,276 @@ $app = new \Slim\Slim();
 
 $app->config(array(
     'debug' => true,
-    'templates.path' => 'templates'
+    
 ));
 
-$app->get('/', function () use ($app, $db) {
+//principal
+$app->get('/', function (){
 	
-	$post = $db->post();
-    
-    $data['post'] = $post->select()->where("estado", 2);
-	$data['imagen'] = $db->media();
 	
+	echo "Api para encuesta!";
 		
-    $app->render('principal.php', $data);
-
-});
-
-$app->get('/theme', function () use ($app) {
-    
     
 
-    $app->render('theme.php');
-
 });
-$app->get('/ingresar', function ( )use ($app){
-	$app->render('ingresar.php');
-}
+
+//devuelve la lista de preguntas....
+$app->get('/pregunta/', function () use($app, $db){
+	$data = array();
+    foreach ($db->pregunta() as $e) {
+        $data[]  = array(
+            "id" => $e["id"],
+            "pregunta" => $e["pregunta"]
+            
+        );
+    }
+	$app->response->headers->set('Content-Type', 'application/json');
+	echo json_encode($data);
 		
-);
-
-$app->get('/salir', function () use ($app){
-	session_destroy();
-	$app->redirect('/');
-});
-
-$app->post('/ingresar', function () use ($app, $db) {
     
-	$request =$app->request;
-	$user = $request->post('user');
-	$password = $request->post('password');
-	
-	
- 	$hasher = new PasswordHash(8, FALSE);
-		
-	if (empty($email) and empty($user)) {
+});
 
-		$app->flash('llenar','llenar todos los campos');
-		
-				
-				
-		} elseif($rows= $db->usuarios()->select("user, pass, id")->where("user", $user)->fetch()) {
-						
-					
-						
-						$hash=$rows['pass'];
-						
-			
-					if ($hasher->CheckPassword($password, $hash)){
-								$_SESSION['username']=$user;
-								$_SESSION['id']=$rows["id"];					
-						$app->redirect('/');
-					}else{
-							$app->flash('error','credenciales invalidas');
-							$app->redirect('ingresar');
-						
-					}	
-			
-			
-			
-			
-		}else{
-			
-
-			$app->flash('error','se produjo un error');
-							$app->redirect('ingresar');
-		}
+$app->post('/pregunta/', function () use($app, $db){
 	
-   
+	$app->response->headers->set('Content-Type', 'application/json');
+	$pregunta = $app->request()->post();
+	$result = $db->pregunta->insert($pregunta);
+	echo json_encode(array("id" => $result["id"]));
 
 });
 
-
-$app->get('/resetPass', function () use ($app){
-	
-			$app->render('resetpass.php');
-			
-});
-
-$app->post('/resetPass', function () use ($app, $db){
-	
-	$request =$app->request;
-	$email = $request->post('email');
-	
-	
-	if(empty($email)){
-		
-		$app->flash('info','llenar el campo');
-		
-	}elseif($user= $db->usuarios()->select()->where("email", $email)->fetch()){
-		
-				 
-			$t_hasher = new PasswordHash(8, FALSE);
-			$nuevapass = generarCodigo(4);
-			$hash = $t_hasher->HashPassword($nuevapass);
-			
-			echo $hash;
-			if($insertado = $user->update(array('pass' => $hash))){
-				echo "<br/>";	
-				echo $nuevapass;
-				
-				$mgClient = new Mailgun('key-9mfpl01wmc4d-18f8bw992ph-j3twli1');
-				$domain = "sandbox9653.mailgun.org";
-
-				# Make the call to the client.
-				$result = $mgClient->sendMessage("$domain",
-				                  array('from'    => 'Querido Usuario <rafael.antonio.martinezs@gmail.com>',
-				                        'to'      => $email,
-				                        'subject' => 'Contraseña nueva',
-				                        'text'    => 'tu nueva pass! '.$nuevapass.' '));
-						
-				$app->redirect('ingresar');
-				exit();
-			}else{
-				
-				echo "no inserto";
-			}
-			
-			
-			
-	}else{
-		
-		echo "no hay resultados";
-	}
-			//$app->render('resetpass.php');
-			
-			
-
+$app->get("/pregunta/:id", function ($id) use ($app, $db) {
+    $app->response()->header("Content-Type", "application/json");
+    $pregunta = $db->pregunta()->where("id", $id);
+    if ($data = $pregunta->fetch()) {
+        echo json_encode(array(
+            "id" => $data["id"],
+            "pregunta" => $data["pregunta"]
+            
+            ));
+    }
+    else{
+        echo json_encode(array(
+            "status" => false,
+            "message" => "encuesta ID $id does not exist"
+            ));
+    }
 });
 
 
-$app->get('/registrar', function () use ($app) {
+$app->put("/pregunta/:id", function ($id) use ($app, $db) {
+    $app->response()->header("Content-Type", "application/json");
+    $pregunta = $db->pregunta()->where("id", $id);
+    if ($pregunta->fetch()) {
+        $post = $app->request()->put();
+        $result = $pregunta->update($post);
+        echo json_encode(array(
+            "status" => (bool)$result,
+            "message" => "Pregunta updated successfully"
+            ));
+    }
+    else{
+        echo json_encode(array(
+            "status" => false,
+            "message" => "Pregunta id $id does not exist"
+        ));
+    }
+});
+
+// rutas opciones
+$app->get('/opcion/', function () use($app, $db){
+	$data = array();	
+    foreach ($db->opcion() as $e) {
+        $data[]  = array(
+            "id" => $e["id"],
+            "opcion" => $e["opcion"],
+            "pregunta_id" => $e["pregunta_id"]
+            
+        );
+    }
+	$app->response->headers->set('Content-Type', 'application/json');
+	echo json_encode($data);
+		
     
-    $app->render('registrar-usuario.php');
+});
+
+$app->post('/opcion/', function () use($app, $db){
+	
+	$app->response->headers->set('Content-Type', 'application/json');
+	$opcion = $app->request()->post();
+	$result = $db->opcion->insert($opcion);
+	echo json_encode(array("id" => $result["id"]));
 
 });
 
-$app->get('/registrarfb',function () use ($app){
-	
-	$app->render('registrarfb.php');
-	
-	
+$app->get("/opcion/:id", function ($id) use ($app, $db) {
+    $app->response()->header("Content-Type", "application/json");
+    $opcion = $db->opcion()->where("id", $id);
+    if ($data = $opcion->fetch()) {
+        echo json_encode(array(
+            "id" => $data["id"],
+            "opcion" => $data["opcion"],
+            "pregunta_id" => $data["pregunta_id"]
+            
+            ));
+    }
+    else{
+        echo json_encode(array(
+            "status" => false,
+            "message" => "Opcion ID $id does not exist"
+            ));
+    }
+});
+
+$app->put("/opcion/:id", function ($id) use ($app, $db) {
+    $app->response()->header("Content-Type", "application/json");
+    $opcion = $db->opcion()->where("id", $id);
+    if ($opcion->fetch()) {
+        $post = $app->request()->put();
+        $result = $opcion->update($post);
+        echo json_encode(array(
+            "status" => (bool)$result,
+            "message" => "Opcion updated successfully"
+            ));
+    }
+    else{
+        echo json_encode(array(
+            "status" => false,
+            "message" => "Opcion id $id does not exist"
+        ));
+    }
 });
 
 
-$app->post('/registrar', function () use ($app, $db){
+// rutas usuario
 
-	$request =$app->request;
-	$username = $request->post('user');
-	$email = $request->post('email');
-	$password = $request->post('password');
-	$t_hasher = new PasswordHash(8, FALSE);
-	$hash = $t_hasher->HashPassword($password);
-	
-	
-
-	
-	
-
-	if (empty($email) and empty($password) and empty($username)) {
-
-		$app->flash('llenar','llenar todos los campos');
+$app->get('/usuario/', function () use($app, $db){
+	$data = array();	
+    foreach ($db->usuario() as $e) {
+        $data[]  = array(
+            "id" => $e["id"],
+            "user" => $e["user"],
+            "email" => $e["email"]
+            
+        );
+    }
+	$app->response->headers->set('Content-Type', 'application/json');
+	echo json_encode($data);
 		
-		
-	}else{
+    
+});
 
-			if ($user= $db->usuarios()->select()->where("user", $username)->fetch()){
-				
-				$app->flash('error', 'ya existe un usuario con ese nombre! porfavor introduzca otro');
-				
-				$app->redirect('registrar');
-					exit();
-				
-			} elseif($user= $db->usuarios()->select()->where("email", $email)->fetch()) {
-				
-				$app->flash('error', 'ya existe ese correo! porfavor introduzca otro');
-				$app->redirect('registrar');
-				exit();
-				
-			}else{
-				
-				if ($insertado = $db->usuarios()->insert(array('user' => $username, 'pass' => $hash, 'email' => $email))) {
-					
-					$app->flash('error', 'se agrego correctamente');
-						$app->redirect('ingresar');
-						exit();
-					
-					
-				} else {
-					$app->flash('error','se produjo un error');
-					$app->redirect('registrar');
-					exit();
-				}
-				
-			}
-			
-			
-		
-
-			
-		}
-
+$app->post('/usuario/', function () use($app, $db){
 	
-
-	});
-
-$app->get('/publicar', function () use ($app){
-	
-	$app->render('publicar.php');
+	$app->response->headers->set('Content-Type', 'application/json');
+	$usuario = $app->request()->post();
+	$result = $db->usuario->insert($usuario);
+	echo json_encode(array("id" => $result["id"]));
 
 });
 
-$app->post('/publicar', function () use ($app, $db){
-	
-	$id=$_SESSION['id'];
-	$request = $app->request;
-	$titulo = $request->post('titulo');
-	$desc = $request->post('descripcion');
-	$img = $_FILES['upimagen']['name'];
-	$tmp_name = $_FILES['upimagen']['tmp_name'];
-	$destino = 'images/'.$id.$img;
-	$videoId = $request->post('yurl');
-	$tipoMedia ="deafult";
-	
-	if(empty($img)){
-		
-		$tipoMedia = "video";
-		
-	}else{
-		
-		$tipoMedia = "imagen";
-	}
-	
-	
-	
-	$tipo = $_FILES['upimagen']['type'];
-	
-	
-	/*
-	echo "Titulo ".$titulo." desc ".$desc." img  ".$img." videoId  ".$videoId." tipo de media ".$tipoMedia."    id     ".$id;
-	*/
-	
-		
-	if ($tipoMedia==="video") {
-		
-			if ($insertadoMedia = $db->media()->insert(array('url' => $videoId,'tipo' => $tipoMedia))) {
-				
-							
-				$lastInsertId = intval($insertadoMedia['id']);
-				
-				
-				
-					if ($insertado = $db->post()->insert(array('titulo' => $titulo, 'descripcion' => $desc,'estado' => 2, 'media_id'=>$lastInsertId, 'usuarios_id' => $id ))) {
-						
-						$app->redirect('publicar');
-					} else {
-						
-						echo "hubo un error al insertar el post";
-						
-					}
-				
-				
-			} else {
-				
-				echo "hubo un error al insertar";
-			}
-		
-		
-	} else {
-		
-				if (!(strpos($tipo, "png") || strpos($tipo, "jpeg") || strpos($tipo, "jpg") ) ){
-						
-						$app->flash('tipo','solo se aceptan extenciones png, jpeg');
-						$app->redirect('publicar');
-						
-				}else{
-									
-						
-								
-								
-							
-								
-					
-							if ($insertadoMedia = $db->media()->insert(array('url' => $destino,'tipo' => $tipoMedia))) {
-									
-									move_uploaded_file($tmp_name, $destino);
-									
-									$lastInsertId = intval($insertadoMedia['id']);
-									
-											if ($insertado = $db->post()->insert(array('titulo' => $titulo, 'descripcion' => $desc,'estado' => 2, 'media_id'=>$lastInsertId, 'usuarios_id' => $id ))) {
-											
-												$app->flash('tipo','se registro correctamente');
-												
-												$app->redirect('publicar');
-													
-											} else {
-												$app->flash('tipo','no se registro');
-												
-											}
-									
-								
-							}else{
-								
-									$app->flash('tipo','error al insertar media');
-								
-							}
-						
-				}	
-						
-				
-			}	
-							
-					
-					
-					
-				
-		
-				
-	
-	
-	
-		
-		
-		
-		
-		
-		
+$app->get("/usuario/:id", function ($id) use ($app, $db) {
+    $app->response()->header("Content-Type", "application/json");
+    $usuario = $db->usuario()->where("id", $id);
+    if ($data = $usuario->fetch()) {
+        echo json_encode(array(
+            "id" => $data["id"],
+            "user" => $data["user"],
+            "email" => $data["email"]
+            
+            ));
+    }
+    else{
+        echo json_encode(array(
+            "status" => false,
+            "message" => "Usuario ID $id does not exist"
+            ));
+    }
 });
 
-$app->get("/post/:id", function ($id) use ($app, $db){
-		
-	$post = $db->post()->where("id", $id);
-	
-	
-	
-	if($data['post'] = $post->fetch()){
-				
-				
-				
-				$mediaId=intval($data['post']['media_id']);
-		
-			$data['related']=$db->post()->select()->where("estado", 2)->limit(3);
-			
-			if($data['media']=$db->media()->select()->where("id",$mediaId)->fetch()){
-				
-				$app->render('post.php', $data);
-		
-			}else{
-				
-				echo "no se encontro ningun resultado para media";
-			}
-		
-		}else{
-			
-			echo "el post no existe";
-		}
-	 
-	
-	});
-	
+/*
+$app->put("/opcion/:id", function ($id) use ($app, $db) {
+    $app->response()->header("Content-Type", "application/json");
+    $opcion = $db->opcion()->where("id", $id);
+    if ($opcion->fetch()) {
+        $post = $app->request()->put();
+        $result = $opcion->update($post);
+        echo json_encode(array(
+            "status" => (bool)$result,
+            "message" => "Opcion updated successfully"
+            ));
+    }
+    else{
+        echo json_encode(array(
+            "status" => false,
+            "message" => "Opcion id $id does not exist"
+        ));
+    }
+});
 
-//zona de perfil de usuario
+*/
 
-$app->get("/perfil", function () use ($app, $db){
+$app->post('/respuesta/', function () use($app, $db){
 	
-	
-	
-	$post=$db->post()->where("usuarios_id", $_SESSION['id']);
-	
-	
-	
-	if (empty($_SESSION['id'])) {
-		
-		 $app->redirect('ingresar');
-		
-	} else {
-		
-			$usuario = $db->usuarios()->where("id", $_SESSION['id']);
-			
-			if ($result['user']=$usuario->fetch()) {
-				
-					$post=$db->post()->where("usuarios_id", $_SESSION['id']);
-						
-						$result['postme']=$post;
-						
-			
-					
-				$app->render('perfil.php', $result);	
-				
-			} else {
-				
-				$resul['error']= "no se ha encontrado el usuario";
-				
-				$app->render('perfil.php', $result);
-			}
-				
-			}
-	
+	$app->response->headers->set('Content-Type', 'application/json');
+	$respuesta = $app->request()->post();
+	$result = $db->respuesta->insert($respuesta);
+	echo json_encode(array("id" => $result["id"]));
 
 });
 
-
-
-$app->get("/videoValidator/:id", function ($id) use ($app){
-	
-	
-	$ids = $id;
-	
-	if(strlen($ids)=='11'){//si es igual a 11 seguimos
-		$url='http://i2.ytimg.com/vi/'.$ids.'/default.jpg';//pasamos el id a url
-			if($conex= @fopen($url,"rt")) {$siono=1;}//si existe hacemos true
-			else{$siono=3;}//si no, false
-	}
-	else{$siono=2;}//si es mayor que 11 false
-		///VERIFICAR ID YOUTUBE///
-		//$app->response->headers->set("Content-Type", "application/json");
-		//echo json_encode($siono);
-	
-	echo $siono;
+$app->get("/respuesta/:id", function ($id) use ($app, $db) {
+    $app->response()->header("Content-Type", "application/json");
+    $respuesta = $db->respuesta()->where("id", $id);
+    if ($data = $respuesta->fetch()) {
+        echo json_encode(array(
+            "id" => $data["id"],
+            "usuario_id" => $data["usuario_id"],
+            "opcion_id" => $data["opcion_id"],
+            "pregunta_id" => $data["pregunta_id"]
+            
+            ));
+    }
+    else{
+        echo json_encode(array(
+            "status" => false,
+            "message" => "Usuario ID $id does not exist"
+            ));
+    }
 });
-$app->post("/videoValidator", function () use ($app){
-	
-	
 
-							
-	});
+// resultado
+$app->post('/resultado/', function () use($app, $db){
+	
+	$app->response->headers->set('Content-Type', 'application/json');
+	$resultado = $app->request()->post();
+	$result = $db->resultado->insert($resultado);
+	echo json_encode(array("id" => $result["id"]));
+
+});
+
+$app->get("/resultado/:id_usuario", function ($usuario_id) use ($app, $db) {
+    $app->response()->header("Content-Type", "application/json");
+    $resultado = $db->resultado()->where("usuario_id", $usuario_id);
+    if ($data = $resultado->fetch()) {
+        echo json_encode(array(
+            "id" => $data["id"],
+            "inicio" => $data["inicio"],
+            "fin" => $data["fin"],
+            "promedio" => $data["promedio"],
+            "usuario_id" => $data["usuario_id"]
+            
+            ));
+    }
+    else{
+        echo json_encode(array(
+            "status" => false,
+            "message" => "Respuesta con Usuario ID $id does not exist"
+            ));
+    }
+});
+
+
 $app->run();
 
 ?>
